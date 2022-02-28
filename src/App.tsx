f26ded2315f5cf9412a32cc146a944be659a08be
JDavidcor23 <jorgedavid23diaz@gmail.com>
2022-02-27 19:06:46 -0500

    termido el agrregar producto y usuario, falta descargar el achivo en texto plano

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useFormik } from "formik";
+import { useState } from "react";
+import TableProducts from './components/TableProducts'
+type User ={
+  usernamme: string,
+  nombreproducto: string,
+  precio:string,
+}
 function App() {
+  const [arrayProducts,setArrayProducts] =useState<User[]>([]);
+  const formik = useFormik<User>({
+    initialValues: {
+      usernamme: "",
+      nombreproducto:"",
+      precio:"",
+    },
+    onSubmit: (data) => {
+       let usernamme = String(data.usernamme)
+       let nombreproducto = String(data.nombreproducto)
+       let precio = String(data.precio)
+       let newProduct: User[]= [...arrayProducts, {usernamme, nombreproducto, precio}]
+        setArrayProducts(newProduct)
+    },
+  });
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{width:"90%", margin:"0 auto", marginTop:"20px"}}>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label">Nombre de usuario</label>
+          <input type="text" 
+          className="form-control" 
+          name="usernamme"
+          onChange={formik.handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label  className="form-label">Nombre del producto</label>
+          <input type="text" className="form-control" 
+          name="nombreproducto"
+          onChange={formik.handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label  className="form-label">Precio</label>
+          <input type="number" 
+          className="form-control" 
+          name="precio"
+          onChange={formik.handleChange}
+          />
+        </div>
+      <button type="submit" className="btn btn-primary">Guardar</button>
+    </form>
+     <TableProducts arrayProducts={arrayProducts}/> 
     </div>
+    </>
   );
 }
 
---
+++ src/components/TableProducts.tsx
@@ -0,0 +1,41 @@
+
+type User ={
+    usernamme: string,
+    nombreproducto: string,
+    precio:string,
+  
+  }
+type ArryProducts={
+    arrayProducts:User[];
+}
+const TableProducts = (arg:ArryProducts) => {
+    return (
+        <div>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Nombre de usuario</th>
+                    <th scope="col">Producto</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Obciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {arg.arrayProducts.map(p => (
+                    <tr key={p.nombreproducto}>  
+                        <td>{p.usernamme}</td>
+                        <td>{p.nombreproducto}</td>
+                        <td>{p.precio}</td>
+                        <td>
+                            <button className="btn btn-warning">Descargar</button>
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table> 
+        </div>
+    );
+};
+
+export default TableProducts;
