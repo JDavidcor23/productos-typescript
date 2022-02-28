cc30bbd00159c2199a9d5fee23bac30c8fb40a96
JDavidcor23 <jorgedavid23diaz@gmail.com>
2022-02-28 00:47:26 -0500

    Se termino el Create, Read y Delete, Falta Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import './styles/modal.css'
 function App() {
+  const generateId = () => Math.random().toString(36)
+  const [products, setProducts]=useState<any[]>([]);
+  const deleteProduct =(id:string)=>{
+    const newProducts = products.filter(p=> p._id.$oid !==id)
+    setProducts(newProducts)
+  }
+  const formik = useFormik({
+    initialValues: {
+      firstName:"",
+      description:"",
+      lastName:"",
+      price:"",
+    },
+    onSubmit: (data) => {
+      const newObjeto ={
+        _id:{
+          $oid:String(generateId())
+        },
+        customer:{
+          firstName:data.firstName,
+          lastName:data.lastName
+        },
+        products:[
+          {description:data.description, price:data.price}
+        ]
+
+
+      }
+      setProducts([...products, {...newObjeto}])
+    },
+  });
+  useEffect(()=>{
+    fetch("http://localhost:3004/results")
+      .then(resp => resp.json())
+      .then(data => setProducts(data))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div style={{width:"90%", margin:"0 auto", marginTop:"20px"}}>
+     {/* -------FOMRULARIO----- */}
+     <h1>Añade tus productos</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label">Nombre</label>
+          <input type="text" 
+          className="form-control" 
+          name="firstName"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label  className="form-label">Apellido</label>
+          <input type="text" className="form-control" 
+          name="lastName"
+          value={formik.initialValues.lastName}
+          onChange={formik.handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label  className="form-label">Nombre del producto</label>
+          <input type="text" 
+          className="form-control" 
+          name="description"
+          value={formik.initialValues.description}
+          onChange={formik.handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label  className="form-label">Precio</label>
+          <input type="number" 
+          className="form-control" 
+          name="price"
+          value={formik.initialValues.price}
+          onChange={formik.handleChange}
+          />
+        </div>
+      <button type="submit" className="btn btn-primary">Guardar</button>
+    </form>
+    {/* -------TABLA----- */}
+    <table className="table" >
+            <thead>
+                <tr>
+                <th scope="col">Cliente</th>
+                <th scope="col">Nombre de producto</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                {products !== undefined &&
+                products.map(p => (
+                <tr key={p._id.$oid}>  
+                    <td>{p.customer.firstName} {p.customer.lastName}</td>
+                    <td>{p.products[0].description}</td>
+                    <td>{p.products[0].price}</td> 
+                    <td style={{display:"flex", flexDirection:"column"}}>
+                        <button className="btn btn-danger" onClick={()=>deleteProduct(p._id.$oid)}>Eliminar</button>
+                        <button className="btn btn-warning" style={{marginTop:"10px"}}>Editar</button>
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+    </table> 
     </div>
   );
 }
